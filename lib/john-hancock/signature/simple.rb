64de6534511983129925175692ccb93a6667506c
Brandon Turner <bturner@bltweb.net>
2011-01-12 18:15:26 -0600

    Added a "simple" signature algorithm

--- lib/john-hancock/signature/simple.rb
+++ lib/john-hancock/signature/simple.rb
@@ -1,5 +1,85 @@
+require 'digest/md5'
+
 module JohnHancock::Signature
+
+  # Signs a URL using three HTTP headers:
+  #   X-Api-Key
+  #   X-Api-Timestamp
+  #   X-Api-Signature
+  #
+  # The signature is the MD5 hash (expressed as a 32 digit hexadecimal number)
+  # of a string comprised of the following elements concatenated together:
+  #
+  #   * Shared Secret
+  #   * Public Key
+  #   * URL path (everything between the domain name and question mark beginning
+  #     with the query string)
+  #   * Current UTC timestamp (seconds since midnight, January 1, 1970)
+  #
+  # For example:
+  #   Api Key:       1234567890
+  #   Shared Secret: MySecret
+  #   Timestamp:     1294870227
+  #   Request:       GET http://api.example.com/foo/bar/1.json?param=test
+  #
+  #   String to hash:
+  #   MySecret1234567890/foo/bar/1.json1294870227
+  #
+  #   Signature:
+  #   2ef24e5b8c1dedc23240fb564874c7a6
+  #
+  #   Headers appended to the HTTP request:
+  #   X-Api-Key: 1234567890
+  #   X-Api-Timestamp: 1294870227
+  #   X-Api-Signature: 2ef24e5b8c1dedc23240fb564874c7a6
+  #
   class Simple < JohnHancock::Signature::Base
-    # TODO
+
+    attr_accessor :key_header, :timestamp_header, :signature_header
+
+    def initialize(request, options={}, &block)
+      @key_header = options.delete(:key_header) || 'X-Api-Key'
+      @timestamp_header = options.delete(:timestamp_header) || 'X-Api-Timestamp'
+      @signature_header = options.delete(:signature_header) || 'X-Api-Signature'
+      super(request, options, &block)
+    end
+
+
+    def signature_base_string
+      "#{secret}#{key}#{path}#{timestamp.to_i}"
+    end
+
+
+    def signature
+      Digest::MD5.hexdigest(signature_base_string)
+    end
+
+    def id_hash
+      {:id => key}
+    end
+
+
+
+    def key
+      request.headers[key_header]
+    end
+
+
+    def timestamp
+      request.headers[timestamp_header]
+    end
+
+
+    def request_signature
+      request.headers[signature_header]
+    end
+
+
+  private
+
+    def path
+      request.path.to_s == '' ? '/' : request.path
+    end
+
   end
 end
---
+++ spec/signature/simple_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+
+module JohnHancock
+  describe Signature::Simple do
+
+    before(:each) do
+      @request = Mock::Request.new({
+        :method => 'GET',
+        :host => 'api.example.com',
+        :headers => {
+          'X-Api-Key' => '1234567890',
+          'X-Api-Timestamp' => '1294870227',
+          'X-Api-Signature' => '2ef24e5b8c1dedc23240fb564874c7a6'
+        },
+        :path => '/foo/bar/1.json',
+        :get => {'param' => 'test'}
+      })
+      @signature = Signature.build(:simple, @request, :secret => 'MySecret')
+    end
+
+
+    it "sets the default header names" do
+      @signature.key_header.should == 'X-Api-Key'
+      @signature.timestamp_header.should == 'X-Api-Timestamp'
+      @signature.signature_header.should == 'X-Api-Signature'
+    end
+
+
+    it "allows changing the header names" do
+      @signature.key_header = 'Api-Id'
+      @signature.key_header.should == 'Api-Id'
+
+      @signature.timestamp_header = 'Api-Timestamp'
+      @signature.timestamp_header.should == 'Api-Timestamp'
+
+      @signature.signature_header = 'Api-Sig'
+      @signature.signature_header.should == 'Api-Sig'
+    end
+
+    it "allows changing the header names via options" do
+      s = Signature::Simple.new(RequestProxy.proxy(@request), {:signature_header => 'Foo-Bar'})
+      s.signature_header.should == 'Foo-Bar'
+    end
+
+    it "fetches the api key" do
+      @signature.key.should == '1234567890'
+    end
+
+
+    it "fetches the timestamp" do
+      @signature.timestamp.to_i.should == 1294870227
+    end
+
+    it "fetches the existing signature" do
+      @signature.request_signature.should == '2ef24e5b8c1dedc23240fb564874c7a6'
+    end
+
+
+    it "composes the signature base string" do
+      @signature.signature_base_string.should == 'MySecret1234567890/foo/bar/1.json1294870227'
+    end
+
+    it "handles empty paths" do
+      @request.options[:path] = ''
+      @signature.signature_base_string.should == 'MySecret1234567890/1294870227'
+    end
+
+    it "generates the signature" do
+      @signature.signature.should == '2ef24e5b8c1dedc23240fb564874c7a6'
+    end
+
+    it "generates the signature with no existing signature" do
+      @request.options[:headers]['X-Api-Signature'] = nil
+      @signature.signature.should == '2ef24e5b8c1dedc23240fb564874c7a6'
+    end
+
+    it "validates an existing correct signature" do
+      @signature.should be_valid_signature
+    end
+
+    it "invalidates an existing incorrect signature" do
+      @request.options[:headers]['X-Api-Signature'] = nil
+      @signature.should_not be_valid_signature
+    end
+
+  end
+end
